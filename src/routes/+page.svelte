55fa693a07dd810a63183fb9957ddb4fb11c26dd
Sean Morgan <46581862+Firesean@users.noreply.github.com>
2024-05-20 08:21:32 -0700

    Adjustments

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
     import CompanyDropdown from "$lib/components/company-dropdown.svelte";
     import FlameDevLogo from "$lib/components/flame-dev-logo.svelte";
     import PreflectLogo from "$lib/components/preflect-logo.svelte";
+    import DesignHedgeLogo from "$lib/components/design-hedge-logo.svelte";
 </script>
 <div class="flex flex-wrap justify-center">
     <!-- <nav class="h-[40px] flex justify-end items-center bg-slate-400">
@@ -18,15 +19,19 @@
     </div> -->
     <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
         <CompanyDropdown> 
-            <InfluentialLogo size="400"/>
+            <InfluentialLogo size="300"/>
         </CompanyDropdown>
 
         <CompanyDropdown> 
-            <PreflectLogo size="400"/>
+            <PreflectLogo size="300"/>
         </CompanyDropdown>
 
         <CompanyDropdown> 
-            <FlameDevLogo size="400"/>
+            <FlameDevLogo size="300"/>
+        </CompanyDropdown>
+
+        <CompanyDropdown> 
+            <DesignHedgeLogo size="300"/>
         </CompanyDropdown>
     </div>
 </div>
