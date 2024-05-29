ab24f71398a7bfe267f90551549615a4b057635e
Sean Morgan <46581862+Firesean@users.noreply.github.com>
2024-05-28 20:35:18 -0700

    Adjustment

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,6 +12,6 @@
 	<title>Flame Development</title>
 </header>
 
-<div class="flex flex-wrap justify-center m-auto bg-gray-800 h-full sm:h-screen w-full p-8 min-w-[400px]">
+<div class="flex flex-wrap justify-center m-auto bg-gray-800 h-full w-full p-8 min-w-[400px]">
 	<slot />
 </div>
