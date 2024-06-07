e0da9fba1aecb62db4f6d9c1ee4d1191f865026a
Sean Morgan <46581862+Firesean@users.noreply.github.com>
2024-06-06 23:12:37 -0700

    Update company-dropdown.svelte

--- src/lib/components/company-dropdown.svelte
+++ src/lib/components/company-dropdown.svelte
@@ -5,22 +5,22 @@
   export let descriptions = [];
 </script>
 
-<div class="relative w-full hover:z-20">
-    <div class="w-full">
-      <div class="group bg-gray-500 w-[30vh] h-[12vh] overflow-hidden flex justify-center items-center transition-all duration-300 hover:h-[40vh] hover:bg-gray-300 rounded-md opacity-80 mx-auto">
-        <div class="relative w-full h-full flex justify-center items-center transition-all duration-300 group-hover:scale-100 scale-[0.5]">
-          <slot />
-          <div class="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center bg-gray-900 bg-opacity-85 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 p-4 gap-2">
-            <h1 class="font-semibold text-md lg:text-xl xl:text-2xl text-wrap text-center self-center">{company}</h1>
-            <h2 class="font-semibold text-sm lg:text-lg xl:text-xl text-wrap text-center self-center text-yellow-400">{title}</h2>
-            <h3 class="font-semibold text-xs lg:text-md xl:text-lg self-start">{length}</h3>
-            <ul class="flex flex-wrap text-wrap justify-start self-start gap-4 text-xs lg:text-sm xl:text-md p-2">
-              {#each descriptions as description}
-                <li>• {description}</li>
-              {/each}
-            </ul>
-          </div>
+<div class="relative w-full overflow-visible">
+  <div class="group relative w-[30vh] h-[12vh] bg-gray-500 rounded-md opacity-80 transition-all duration-300 hover:z-20">
+    <div class="overflow-hidden relative w-full h-full flex justify-center items-center transition-all duration-300 group-hover:absolute group-hover:top-0 group-hover:left-0 group-hover:w-[30vh] group-hover:h-[40vh] group-hover:bg-gray-300 group-hover:overflow-visible rounded-md">
+      <div class="hover:overflow-hidden relative w-full h-full flex justify-center items-center transition-transform duration-300 scale-[0.5] group-hover:scale-100">
+        <slot />
+        <div class="absolute inset-0 flex flex-col justify-center items-center bg-gray-900 bg-opacity-85 text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 p-4 gap-2">
+          <h1 class="font-semibold text-center">{company}</h1>
+          <h2 class="font-semibold text-center text-yellow-400">{title}</h2>
+          <h3 class="font-semibold self-start">{length}</h3>
+          <ul class="flex flex-wrap justify-start gap-4 p-2 ">
+            {#each descriptions as description}
+              <li>• {description}</li>
+            {/each}
+          </ul>
         </div>
       </div>
     </div>
+  </div>
 </div>
