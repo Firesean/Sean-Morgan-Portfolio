a951a3d05291ff01623b3f4ce771bee4e12f51a3
Sean Morgan <46581862+Firesean@users.noreply.github.com>
2024-06-23 20:57:19 -0700

    Navigation Bar Adjustment

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,8 @@
 	let options = [
 		"Skills",
 		"Experience",
-		"About"
+		"About",
+		"Contact"
 	];
 </script>
 
@@ -19,12 +20,14 @@
 </header>
 
 
-<nav class="flex flex-nowrap gap-32 bg-white opacity-60 h-[5vh] justify-center items-center p-4 fixed w-full z-10">
+<nav class="flex flex-nowrap gap-[4vw] bg-white opacity-60 h-[5vh] justify-center items-center p-4 fixed w-full z-10">
 	
 		{#each options as option}
-			<div>
-				{option}
-			</div>
+			<a href="./{option.toLowerCase()}">
+				<div class="px-[2vw] text-lg font-semibold">
+					{option}
+				</div>
+			</a>
 		{/each}
 </nav>
 <div class="flex flex-wrap justify-center m-auto bg-gray-800 h-full w-full p-8 min-w-[400px]">
