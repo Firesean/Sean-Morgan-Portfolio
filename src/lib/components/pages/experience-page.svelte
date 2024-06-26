38e37ff19d85064b5bd09fb2aeb07461f9178dac
Sean Morgan <46581862+Firesean@users.noreply.github.com>
2024-06-26 00:34:07 -0700

    Phone Adjustment

--- src/lib/components/pages/experience-page.svelte
+++ src/lib/components/pages/experience-page.svelte
@@ -7,6 +7,8 @@
     import OpportunityVillageLogo from "$lib/components/opportunity-village-logo.svelte";
     import MiniGranPrixLogo from "$lib/components/mini-gran-prix-logo.svelte";
 
+    const userAgent = navigator.userAgent;
+
     let companies = {
         "Mini Gran Prix" : {
             "title": "Ride Attendant",
@@ -97,7 +99,7 @@
 </script>
 
 
-<div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 self-center pb-[15em]">
+<div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 self-center pb-[15em] {/Mobile/.test(userAgent) ? '!grid-cols-1': ''}">
     {#each Object.entries(companies) as details, index}
         <CompanyDropdown company={details[0]} title={details[1].title} length={details[1].length} descriptions={details[1].descriptions}>
             {#if details[0] == "Flame Development"}
