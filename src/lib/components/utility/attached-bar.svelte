b68cbc48a75d7edeeee1dfdd1ca3f1453fa7c2ec
Sean Morgan <46581862+Firesean@users.noreply.github.com>
2024-07-06 23:53:24 -0700

    Font Size Adjustment on About Page

--- src/lib/components/utility/attached-bar.svelte
+++ src/lib/components/utility/attached-bar.svelte
@@ -9,7 +9,7 @@
     <div class="flex flex-col {attachedSide == "left" ? "justify-start" : "justify-end"} w-[80%]">
         <h2 class="{attachedSide == "left" ? "self-end pr-8" : "self-start pl-8"}">{title}</h2>
         <div class="bg-white flex w-full p-4 {attachedSide == "left" ? "pl-0" : "pr-0"} rounded-lg relative right-0" style="height: {infoBarHeight};">
-            <div class="h-full bg-gray-800 self-center w-full break-words flex {attachedSide == "left" ? "justify-end" : "justify-start"} p-4 overflow-y-scroll">
+            <div class="h-full bg-gray-800 self-center w-full break-words flex {attachedSide == "left" ? "justify-end" : "justify-start"} p-4 overflow-hidden">
                 <slot />
             </div>
         </div>
