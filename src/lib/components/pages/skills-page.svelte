b105f74148ed5498b8ddda0d463a2e213d4ee6e8
Sean Morgan <46581862+Firesean@users.noreply.github.com>
2024-07-07 10:24:12 -0700

    Temp Spacing for certain pages

--- src/lib/components/pages/skills-page.svelte
+++ src/lib/components/pages/skills-page.svelte
@@ -3,16 +3,10 @@
 
 </script>
 
-<div class="h-full pb-[32em]">
-    <div class="flex flex-row flex-wrap justify-center mt-[-100px] mx-auto gap-[8em]">
+<div class="h-full pb-[72em]">
+    <div class="flex flex-row flex-wrap justify-center mt-[-100px] mx-auto">
         <div class="flex flex-row flex-wrap gap-16">
-            {#each Array(4) as item, i}
-                <SkillsContainer index={i}/>
-            {/each}
-        </div>
-
-        <div class="flex flex-row flex-wrap gap-16">
-            {#each Array(4) as item, i}
+            {#each Array(8) as item, i}
                 <SkillsContainer index={i}/>
             {/each}
         </div>
