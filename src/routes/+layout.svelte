414f28bff4e088c29dc592052f3782ccb6381798
Sean Morgan <46581862+Firesean@users.noreply.github.com>
2024-07-07 10:18:04 -0700

    Light and Dark Mode Button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,18 +2,27 @@
 	import "../app.css";
 	import "../light-mode.css";
 	import "../dark-mode.css";
-	let mode = "dark";
+    import SunIcon from "$lib/components/icons/sun-icon.svelte";
+    import MoonIcon from "$lib/components/icons/moon-icon.svelte";
+
+	let modes = ["dark", "light"];
+	let selectedMode = 0;
+
+	function cycleMode() {
+		selectedMode = (selectedMode + 1) % modes.length;
+	}
 </script>
-
-<style lang="postcss">
-
-</style>
 
 <header>
 	<title>Portfolio Page</title>
 </header>
 
-<div class="flex flex-wrap justify-center m-auto h-full w-full min-w-[400px] {mode == "dark" ? "dark" : "light"}">
+<div class="flex flex-wrap justify-center m-auto h-full w-full min-w-[400px] {modes[selectedMode]}">
 	<slot />
-	<button class="text-red-600 fixed bottom-12 right-12" on:click={() => {mode == "dark" ? mode = "light" : mode = "dark"}}>Change Mode</button>
+	<button class="text-red-600 fixed bottom-16 right-16" on:click={cycleMode}>
+		<div class="fixed">
+			<span class="fixed {modes[selectedMode] == "dark" ? 'opacity-100' : "opacity-0"}"><MoonIcon stroke="#FFF" size=40 /></span>
+			<span class="fixed {modes[selectedMode] == "light" ? 'opacity-100' : "opacity-0"}"><SunIcon stroke="#000" size=40 /></span>
+		</div>
+	  </button>
 </div>
