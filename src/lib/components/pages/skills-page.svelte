b0ce7b60d7d6d571f5087bb2080e4b93f8566d44
Sean Morgan <46581862+Firesean@users.noreply.github.com>
2024-07-07 09:43:44 -0700

    Minor Light and Dark Mode Adjustment

--- src/lib/components/pages/skills-page.svelte
+++ src/lib/components/pages/skills-page.svelte
@@ -3,7 +3,7 @@
 
 </script>
 
-<div class="h-full pb-[40em]">
+<div class="h-full pb-[32em]">
     <div class="flex flex-row flex-wrap justify-center mt-[-100px] mx-auto gap-[8em]">
         <div class="flex flex-row flex-wrap gap-16">
             {#each Array(4) as item, i}
