63f8cbbfa698b09786d281c5733e59bd4844a591
Sean Morgan <46581862+Firesean@users.noreply.github.com>
2024-07-12 09:48:12 -0700

    Attached Bar Adjustments

--- src/lib/components/pages/about-page.svelte
+++ src/lib/components/pages/about-page.svelte
@@ -2,15 +2,13 @@
     import AttachedBar from "$lib/components/utility/attached-bar.svelte";
 
     let infoBarHeight = "350px";
-    let sections = ["Introduction", "Education", "Professional Summary", "Interests", "Self Development"];
 
-
-    let aboutData = {
+    let sections = {
         "Introduction" : [
-            "Introduced to programming in 2010.",
-            "Dual Associates from College of Southern Nevada in Computer and Information Technology [Science]",
-            "In 2020, hired as a Quality Assurance Analyst at Influential Network",
-            "In 2023, joined Preflect Ads as a Software Engineer"
+            "You have a problem, a have a solution",
+            "Web Application, Automation, Scraping, IOT, I tinker in it all",
+            "Enjoy learning and exploring new terrian",
+            "I seek growth in developing my skills"
         ],
         "Education" : [
             "Dual Associates in Computing and Information Science",
@@ -19,19 +17,25 @@
             "Western Governors University"
         ],
         "Professional Summary" : [
-            "Problem Solver: Good at solving technical issues and improving software",
-            "Team Player: Works well with teams and delivers quality software on time",
-            "Always Learning: Keeps up with new tech trends and improves skills",
-            "Full Stack: Experienced in front-end and back-end development for software performance",
+            "Problem Solver: Analytical and observate, reaching point A to point B",
+            "Team Player: Everyone has a spot they belong",
+            "Always Learning: New tech advancements and skill improvement",
+            "Full Stack: Front-end and Back-end creations",
+        ],
+        "Interests" : [
+            "Actitivies: Hockey / Tennis / Wrestling / Biking",
+            "Video Games:  Dead By Daylight / CSGO / Rainbow 6 Siege / Yugioh / Pokemon",
+            "Dancing: Break Dance / Hip-Hop / Line Dancing / (West Coast / Country) Swing",
+            "Music: Rock / Metal / Country / Rap"
         ]
     }
 
 </script>
 
 <div class="w-full flex flex-col gap-16">
-    {#each Object.keys(aboutData) as title, i}
-        <AttachedBar bind:infoBarHeight={infoBarHeight} title={title} class="attached-bar" attachedSide={i % 2 == 0 ? "left" : "right"}>
-            {#each aboutData[title] as detail, j}
+    {#each Object.keys(sections) as title, i}
+        <AttachedBar bind:infoBarHeight={infoBarHeight} title={title} attachedSide={i % 2 == 0 ? "left" : "right"}>
+            {#each sections[title] as detail, j}
                 <li class:highlight={j % 2 != 0}>
                     {detail}
                 </li>
--- src/lib/components/utility/attached-bar.svelte
+++ src/lib/components/utility/attached-bar.svelte
@@ -2,15 +2,16 @@
     export let infoBarHeight;
     export let title;
     export let attachedSide = "left";
+
+    import WelcomeSignIcon from "$lib/components/icons/welcome-sign-icon.svelte";
 </script>
 
-
-<div class="flex flex-col {attachedSide == "left" ? "items-start self-start" : "items-end self-end"} w-full" style="font-size: clamp(.6em, 2vw, 1.5em);">
+<div class="flex flex-col {attachedSide == "left" ? "items-start self-start" : "items-end self-end"} w-full min-w-[400px]" style="font-size: clamp(.85em, 1.5vw, 2em);">
     <div class="flex flex-col {attachedSide == "left" ? "justify-start" : "justify-end"} w-[80%]">
         <h2 class="{attachedSide == "left" ? "self-end pr-4" : "self-start pl-4"}">{title}</h2>
         <div class="attached-bar-outer flex w-full p-4 {attachedSide == "left" ? "pl-0" : "pr-0"} rounded-lg relative right-0" style="height: {infoBarHeight};">
-            <div class="h-full attached-bar-inner self-center w-full break-words flex {attachedSide == "left" ? "justify-end text-right" : "justify-start text-left"} p-4 overflow-hidden">
-                <div class="flex flex-col flex-wrap w-full gap-4 justify-between">
+            <div class="h-full attached-bar-inner self-center w-full break-words flex overflow-hidden {attachedSide == "left" ? "flex-row" : "flex-row-reverse"}">
+                <div class="flex flex-col flex-wrap w-full gap-4 justify-center items-center {attachedSide == "left" ? "-rotate-6 -translate-x-6" : "rotate-6 translate-x-6"}">
                     <slot />
                 </div>
             </div>
