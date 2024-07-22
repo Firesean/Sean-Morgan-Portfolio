35567d47b826cc7f6bd73c092a953e2ff75411ad
Sean Morgan <46581862+Firesean@users.noreply.github.com>
2024-07-22 09:59:09 -0700

    Added Shit Resume Icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,7 @@
 	import "../dark-mode.css";
     import SunIcon from "$lib/components/icons/sun-icon.svelte";
     import MoonIcon from "$lib/components/icons/moon-icon.svelte";
+	import ResumeIcon from "$lib/components/icons/resume-icon.svelte";
     import Resume from "$lib/components/resume.svelte";
 
 	import { onMount } from "svelte";
@@ -29,12 +30,15 @@
 
 <div class="flex flex-wrap justify-center m-auto h-full w-full min-w-[400px] {modes[selectedMode]} print:hidden">
 	<slot />
+	<button class="fixed bottom-32 right-16" on:click={() => {window.print(); return false;}}>
+		<span class="fixed {modes[selectedMode] == "dark" ? 'opacity-100' : "opacity-0"}"><ResumeIcon fill="#FFF" size=40 /></span>
+	</button>
 	<button class="text-red-600 fixed bottom-16 right-16" on:click={cycleMode}>
 		<div class="fixed">
 			<span class="fixed {modes[selectedMode] == "dark" ? 'opacity-100' : "opacity-0"}"><MoonIcon stroke="#FFF" size=40 /></span>
 			<span class="fixed {modes[selectedMode] == "light" ? 'opacity-100' : "opacity-0"}"><SunIcon stroke="#000" size=40 /></span>
 		</div>
-	  </button>
+	</button>
 </div>
 
 <Resume />
