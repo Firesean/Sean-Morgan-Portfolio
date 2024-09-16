19b9f10be2e0054a6dcf35326004907b2c1462d8
Sean Morgan <46581862+Firesean@users.noreply.github.com>
2024-09-16 12:07:49 -0700

    QR Test

---
+++ src/routes/resume/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { onMount } from 'svelte';
+  
+    // Path to your PDF file in the static folder
+    const pdfUrl = 'static/assets/files/Portfolio-Page.pdf';
+  
+    function downloadPDF() {
+      const link = document.createElement('a');
+      link.href = pdfUrl;
+      link.download = 'Resume.pdf'; 
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
+  
+    onMount(() => {
+      downloadPDF();
+    });
+</script>
